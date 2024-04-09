113158ccaf55281eba3f20b033a9afb42b674f43
Diego Malta <diego_moreira.0791@outlook.com>
2024-04-09 03:37:42 -0300

    add february path

--- challenges/views.py
+++ challenges/views.py
@@ -3,5 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
- return HttpResponse("<h1>Hello, world. You're at the challenges index.</h1>")
+def january(request):
+ return HttpResponse("<h1>Hello, world. You're at the challenges for January.</h1>")
+
+def february(request):
+ return HttpResponse("<h1>You're on the February challenge.</h1>")
