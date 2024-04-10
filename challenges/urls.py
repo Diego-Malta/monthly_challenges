3c766a4da7d32edde3ae8101a380624f127597dc
Diego Malta <diego_moreira.0791@outlook.com>
2024-04-10 15:26:55 -0300

    use a dic and redirect urls with ints as responses

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  # path("january", views.january),
-  # path("february", views.february),
-  path("<month>", views.monthly_challenge)
+  path("<int:month>", views.monthly_challenge_by_number),
+  path("<str:month>", views.monthly_challenge)
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,13 +1,36 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
 # Create your views here.
 
-def monthly_challenge(request, month):
-  months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
+monthly_challenges = {
+  "january": "Eat no meat for the entire month!",
+  "february": "Walk for at least 20 minutes every day!",
+  "march": "Learn Django for at least 20 minutes every day!",
+  "april": "Start a new Django project.",
+  "may": "Walk for at least 20 minutes every day!",
+  "june": "Learn Django for at least 20 minutes every day!",
+  "july": "Eat no meat for the entire month!",
+  "august": "Walk for at least 20 minutes every day!",
+  "september": "Learn Django for at least 20 minutes every day!",
+  "october": "Walk for at least 20 minutes every day!",
+  "november": "Learn Python",
+  "december": "Learn Django for at least 20 minutes every day!"
+}
+
+def monthly_challenge_by_number(request, month):
+  months = list(monthly_challenges.keys())
   
-  if month not in months:
-    # return HttpResponse(f"<h1>Invalid month.</h1>")
+  if month < len(months):
+    return HttpResponseNotFound("<h1>Invalid month</h1>")
+  
+  redirect_month = months[month - 1]
+  return HttpResponseRedirect("/challenges/" + redirect_month)
+
+def monthly_challenge(request, month):
+  try:
+    challenge_text = monthly_challenges[month]
+  except:
     return HttpResponseNotFound("<h1>Invalid month.</h1>")
-  else:
-    return HttpResponse(f"<h1>You're on the {month} challenge.</h1>")
+  
+  return HttpResponse(f"<h1>{challenge_text}</h1>")
