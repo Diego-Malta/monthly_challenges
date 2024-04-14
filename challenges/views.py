f9a18ec642a9b9609a0709aaf0f3e6a70ad734a2
Diego Malta <diego_moreira.0791@outlook.com>
2024-04-14 14:25:29 -0300

    add reverse function for dynamic paths and returning html

--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 
@@ -21,16 +22,18 @@ monthly_challenges = {
 def monthly_challenge_by_number(request, month):
   months = list(monthly_challenges.keys())
   
-  if month < len(months):
+  if month > len(months):
     return HttpResponseNotFound("<h1>Invalid month</h1>")
   
   redirect_month = months[month - 1]
-  return HttpResponseRedirect("/challenges/" + redirect_month)
+  redirect_path = reverse("month-challenge", args=[redirect_month])
+  return HttpResponseRedirect(redirect_path)
 
 def monthly_challenge(request, month):
   try:
     challenge_text = monthly_challenges[month]
+    response_data = f"<h1>{challenge_text}</h1>"
   except:
     return HttpResponseNotFound("<h1>Invalid month.</h1>")
   
-  return HttpResponse(f"<h1>{challenge_text}</h1>")
+  return HttpResponse(response_data)
